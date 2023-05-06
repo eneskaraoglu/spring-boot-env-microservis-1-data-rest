c66bff8d5903a3b6945b8733cbb1ac28e20dbc7e
enes k <eneskaraoglu@gmail.com>
2023-05-06 23:13:23 +0300

    init commit
    init commit

---
+++ src/main/java/com/eneskaraoglu/ek/Micro1DataRest.java
@@ -0,0 +1,13 @@
+package com.eneskaraoglu.ek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro1DataRest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro1DataRest.class, args);
+	}
+
+}
